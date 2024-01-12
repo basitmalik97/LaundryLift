912c121d515a61e05484fa50f33956a176336227
unknown <anthonyaadamson@gmail.com>
2024-01-11 20:28:03 -0500

    reactify about.jsx and services.jsx

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,43 +1,30 @@
 const About = () => {
     return (
-        
-
-
-<html>
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" type="text/css" href="style.css">
-<title>About Us!</title>
-</head>
-<body>
-
-
-<div class="wrapper">
-    <div class="testimonial">
+<div className="wrapper about-all">
+    <div className="testimonial">
         <article>
             <head>
                 <h1>About us</h1>
                 <img src="about us.png"
                 width="200"
-                height="200">
+                height="200" />
 
-                <br> 
+                <br /> 
                 <blockquote>
                     Welcome to Laundry Lift, where we take care of your garments with precision and care. Our seamless laundry process is designed to make your life easier, offering a hassle-free experience from start to finish. Explore our range of services below:
                                                                            
                 </blockquote>
-                <br> 
+                <br /> 
                 <blockquote>
                     1. Place an Order
 Getting started is as easy as a click! Place an order through our user-friendly platform, specifying your preferences and laundry needs. Select from our range of services, including washing, folding, ironing, and dry cleaning.
                 </blockquote>
-                <br>
+                <br />
                 <blockquote>
                     2. Convenient Pick-Up
 Once your order is placed, we make laundry day even easier by picking up your garments right from your doorstep. Our efficient and friendly team ensures a smooth and hassle-free pick-up experience, saving you valuable time.
                 </blockquote>
-                <br>
+                <br />
                 <blockquote>
                     3. Expert Cleaning
                     Leave the dirty work to us! Our washing, folding, ironing, and dry cleaning services are executed with precision and care. We use advanced technology, premium detergents, and industry-leading techniques to ensure your garments receive the treatment they deserve. 
@@ -50,7 +37,7 @@ Once your order is placed, we make laundry day even easier by picking up your ga
                 4. Hassle-Free Delivery
 Sit back and relax as we bring your freshly cleaned and neatly folded laundry back to you. Our reliable delivery service ensures that your garments are returned to your doorstep in a timely manner, ready to be enjoyed.
                </blockquote>
-               <br> 
+               <br /> 
                
                <div>
 
@@ -58,22 +45,16 @@ Sit back and relax as we bring your freshly cleaned and neatly folded laundry ba
                <blockquote>
                 At Laundry Lift, we understand the importance of clean, well-maintained clothing. Our services are tailored to meet your specific needs, providing you with the convenience and peace of mind you deserve. Trust us with your laundry, and let us elevate your laundry experience.
                </blockquote>
-               <br> 
+               <br /> 
                
                <div>
-                <br> 
+               <br /> 
                </div>
                
-               <hr>
             </head>
         </article>
-    
-
     </div>
 </div>
-
-
-
     );
 };
 export default About
