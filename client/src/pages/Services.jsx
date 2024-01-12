cc8610c2c28b6f31b838734e12fc80c66359732d
basitmalik97 <basitmalik97@gmail.com>
2024-01-11 19:54:44 -0500

    add images and service page

--- client/src/pages/Services.jsx
+++ client/src/pages/Services.jsx
@@ -2,7 +2,231 @@ import '../styles/Services.css';
 
 const Services = () => {
     return (
-        <h1>Services</h1>
-    );
+        
+        <body>
+        
+            <div class="gradient-section">
+        
+                <div class="bg-container">
+        
+        
+                    <!-- Nav/Header Section -->
+        
+                    <header>
+                        <div class="logo">
+                            <a href="#">
+                                <img src="./images/Logo.png" alt="Logo"> Laundry Lift
+                            </a>
+                        </div>
+        
+                        <nav>
+        
+        
+                            <div class="navbutton">
+                                <a href="#">Home</a>
+                            </div>
+        
+                            <div class="navbutton">
+                                <a href="#">About Us</a>
+                            </div>
+        
+                            <div class="navbutton">
+                                <a href="#">Services</a>
+                            </div>
+        
+                            <div class="contactusnavbutton">
+                                <a href="#">Contact Us</a>
+                            </div>
+        
+                            <div class="loginnavbutton">
+                                <a href="#">Login</a>
+                            </div>
+        
+        
+                        </nav>
+                    </header>
+        
+                    <!-- Main Section -->
+        
+                    <main>
+                        <div class="herobox1">
+        
+                            <h1>Our Laundry Services</h1>
+                            <p class="main-p">Welcome to Laundry Lift, where we take care of your garments with precision and
+                                care. Our
+                                comprehensive laundry
+                                services are designed to make your life easier, giving you more time to focus on what matters
+                                most. Explore our
+                                range of services below:</p> <br>
+                        </div>
+                    </main>
+                    <!-- Services Section -->
+        
+                    <div class="service-section">
+        
+                        <div class="herobox2">
+        
+                            <h2>Washing</h2>
+                            <p>Leave the dirty work to us! Our washing service ensures that your clothes are treated with the
+                                utmost care. We use
+                                advanced technology and premium detergents to remove stains, odors, and impurities, leaving your
+                                garments fresh and
+                                vibrant.</p> <br>
+                            <a href="test-page.html" class="mybutton">Book Now</a>
+                        </div>
+        
+                        <div class="herobox2-img">
+                            <img src="./images/Washing.jpg" alt="Image for washing.">
+                        </div>
+                    </div>
+        
+                    <div class="service-section">
+        
+                        <div class="herobox3">
+        
+                            <h2>Ironing</h2>
+                            <p>Experience the joy of neatly folded laundry without lifting a finger. Our meticulous folding
+                                service not only saves
+                                you time but also ensures that your clothes are organized and ready to be placed back into your
+                                wardrobe with
+                                precision.</p> <br>
+                            <a href="test-page.html" class="mybutton">Book Now</a>
+                        </div>
+        
+                        <div class="herobox3-img">
+                            <img src="./images/Ironing.jpg" alt="Image for Ironing.">
+                        </div>
+        
+                    </div>
+        
+                    <div class="service-section">
+        
+                        <div class="herobox4">
+        
+                            <h2>Folding</h2>
+                            <p>Say goodbye to wrinkles and creases! Our ironing service transforms your clothes into impeccably
+                                pressed
+                                masterpieces. Whether it's business attire or casual wear, we handle each garment with expertise
+                                to deliver a
+                                polished and professional look.</p> <br>
+                            <a href="test-page.html" class="mybutton">Book Now</a>
+                        </div>
+        
+                        <div class="herobox4-img">
+                            <img src="./images/Folding.jpg" alt="Image for Folding.">
+                        </div>
+        
+                    </div>
+        
+                    <div class="service-section">
+        
+                        <div class="herobox5">
+        
+                            <h2>Dry cleaning</h2>
+                            <p>For delicate fabrics and garments with special care requirements, our dry cleaning service is the
+                                solution. Using
+                                industry-leading techniques and eco-friendly solvents, we gently clean and refresh your items,
+                                preserving their
+                                quality and extending their lifespan.</p> <br>
+                            <a href="test-page.html" class="mybutton">Book Now</a>
+                        </div>
+        
+                        <div class="herobox5-img">
+                            <img src="./images/Dry-cleaning.jpg" alt="Image for dry cleaning.">
+                        </div>
+        
+                    </div>
+        
+                    <div class="service-section">
+        
+                        <div class="herobox6">
+        
+                            <h2>Wash & Fold</h2>
+                            <p>Enjoy the perfect combination of convenience and quality with our Wash & Fold service. Simply
+                                drop off your laundry,
+                                and our expert team will take care of the rest. Your clothes will be washed, dried, and expertly
+                                folded, ready for
+                                you to pick up or have delivered to your doorstep. Experience the ultimate time-saving solution
+                                for your busy
+                                lifestyle.</p> <br>
+                            <a href="test-page.html" class="mybutton">Book Now</a>
+                        </div>
+        
+                        <div class="herobox6-img">
+                            <img src="./images/Wash-fold.jpg" alt="Image for wash & fold.">
+                        </div>
+        
+                    </div>
+        
+                    <div class="service-section">
+        
+                        <div class="herobox7">
+        
+                            <h2>Wash & Iron</h2>
+                            <p>For a hassle-free laundry routine, try our Wash & Iron service. We go the extra mile by not only
+                                washing your clothes
+                                with precision but also ensuring they are professionally ironed to perfection. Whether it's your
+                                everyday essentials
+                                or special garments, our team ensures that your clothes look impeccable and ready to wear.</p>
+                            <br>
+                            <a href="test-page.html" class="mybutton">Book Now</a>
+                        </div>
+        
+                        <div class="herobox7-img">
+                            <img src="./images/Wash-Iron.webp" alt="Image for wash & iron.">
+                        </div>
+        
+                    </div>
+        
+                    <!-- Footer Section -->
+                    <footer>
+                        <div class="footer-social">
+                            <a href="#" target="_blank" class="fa fa-facebook"></a>
+                            <a href="#" target="_blank" class="fa fa-twitter"></a>
+                            <a href="#" target="_blank" class="fa fa-instagram"></a>
+                            <br>
+                            <br>
+                        </div>
+        
+                        <div class="footer-nav">
+        
+                            <div class="footer-navbutton">
+                                <a href="#">Home</a>
+                            </div>
+        
+                            <div class="footer-navbutton">
+                                <a href="#">About Us</a>
+                            </div>
+        
+                            <div class="footer-navbutton">
+                                <a href="#">Services</a>
+                            </div>
+        
+                            <div class="footer-navbutton">
+                                <a href="#">Contact Us</a>
+                            </div>
+        
+                            <div class="footer-navbutton">
+                                <a href="#">Login</a>
+                            </div>
+                        </div>
+        
+        
+        
+                        <div class="footer-bottom">
+                            All rights reserved - Copyright 2023©
+        
+                        </div>
+        
+                    </footer>
+                </div>
+        
+        
+        
+            </div>
+        
+        </body>
+        
+        </html>    );
 };
 export default Services
