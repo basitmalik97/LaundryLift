79386587c9fe7f000dca719ad8291d66b050692c
basitmalik97 <139717262+basitmalik97@users.noreply.github.com>
2024-01-18 18:29:03 -0500

    fix port

--- server/server.js
+++ server/server.js
@@ -40,7 +40,6 @@ const startApolloServer = async () => {
 
   db.once('open', () => {
       app.listen(process.env.PORT || 5000, () => {
-      const port = server.address().port;
       console.log(`Express is working on port ${port}`);
     });
 };
